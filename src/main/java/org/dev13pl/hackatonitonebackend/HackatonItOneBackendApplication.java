029e46bd206aa5d84179e3a87a63797168eae8db
Ivan Ilin <Ivan3nd3r11@gmail.com>
2024-05-18 11:51:01 +0300

    Initial commit

---
+++ src/main/java/org/dev13pl/hackatonitonebackend/HackatonItOneBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dev13pl.hackatonitonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonItOneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonItOneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dev13pl/hackatonitonebackend/HackatonItOneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev13pl.hackatonitonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonItOneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
